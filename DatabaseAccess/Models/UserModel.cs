e1aed865d542ae10731c9bfe05a69b8f3dd7066e
IulianCoDu <iulian.co.du@gmail.com>
2020-03-08 22:48:31 +0200

    Add project files.

---
+++ DatabaseAccess/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public UserModel(int userId, string firstName, string lastName, string password, string email)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Email = email;
+        }
+    }
+}
---
+++ DatabaseAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Repository
+{
+    public class UnitOfWork
+    {
+        private EFDBEntities context = new EFDBEntities();
+        public void SaveContext()
+        {
+            context.SaveChanges();
+        }
+        public void DisposeContext()
+        {
+            context.Dispose();
+        }
+    }
+}
