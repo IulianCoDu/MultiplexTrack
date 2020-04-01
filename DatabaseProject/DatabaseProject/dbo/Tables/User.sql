5846d5b64063f04935460248bf4828758462293a
IulianCoDu <iulian.co.du@gmail.com>
2020-04-01 23:31:47 +0300

    Update DB via DBProject with User Column

--- DatabaseProject/DatabaseProject/dbo/Tables/User.sql
+++ DatabaseProject/DatabaseProject/dbo/Tables/User.sql
@@ -2,6 +2,7 @@
     [Id]        INT          IDENTITY (1, 1) NOT NULL,
     [FirstName] VARCHAR (50) NULL,
     [LastName]  VARCHAR (50) NULL,
+    [User]  VARCHAR (50) NULL,
     [Password]  VARCHAR (50) NULL,
     [Email]     VARCHAR (50) NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
