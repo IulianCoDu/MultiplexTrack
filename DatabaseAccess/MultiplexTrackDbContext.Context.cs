3fff039d28611c96ddc66612417cabb6de72b56e
Duduveche Constantin-Iulian <iulian.co.du@gmail.com>
2020-08-20 21:37:59 +0300

    Update name of context

--- DatabaseAccess/MultiplexTrackDbContext.Context.cs
+++ DatabaseAccess/MultiplexTrackDbContext.Context.cs
@@ -13,10 +13,10 @@ namespace DatabaseAccess
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class MultiplexTrackDbContext : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public MultiplexTrackDbContext()
+            : base("name=MultiplexTrackDbContext")
         {
         }
     
